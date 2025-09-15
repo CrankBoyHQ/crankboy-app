ec9b76c8db74567f2df0d9344b034ed993187056
nstbayless <nstbayless@gmail.com>
2025-09-14 19:11:58 -0700

    renamed uncapped fps -> turbo speed

--- src/scenes/settings_scene.c
+++ src/scenes/settings_scene.c
@@ -1289,10 +1289,10 @@ static OptionsMenuEntry* getOptionsEntries(CB_SettingsScene* scene)
 
     // uncap fps
     entries[++i] = (OptionsMenuEntry){
-        .name = "Uncapped FPS",
+        .name = "Turbo Speed",
         .values = off_on_labels,
         .description =
-            "Removes the speed limit.\n \nThis is intended\njust for benchmarking\nperformance, not for\ncasual play."
+            "Removes the FPS limit.\n \nThis is intended\njust for benchmarking\nperformance, not for\ncasual play."
         ,
         .pref_var = &preferences_uncap_fps,
         .max_value = 2,
